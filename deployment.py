4ff1f68a9593ee623ff5a942502bd9461682cf7e
Mohammed-Zwam <mohammed.ashraf.zwam@gmail.com>
2024-07-03 15:17:20 +0300

    New Version

--- deployment.py
+++ deployment.py
@@ -4,9 +4,15 @@ import streamlit as st
 from sklearn.naive_bayes import MultinomialNB
 import pickle
 import PIL as img
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
 # Upload Data
-model = pickle.load(open('D:\Programming\Coding\Python [AI]\Sentiment Analysis Project\SentimentFile.sav','rb'))
+with open('D:\Programming\Coding\Python [AI]\Sentiment-Analysis-Project\SentimentFile.sav', 'rb') as file:
+    model = pickle.load(file)
+with open('D:\Programming\Coding\Python [AI]\Sentiment-Analysis-Project\Bag of Words.sav', 'rb') as file:
+    cv = pickle.load(file)
+
 st.title('Sentiment Analysis Project')
 st.info('This project focuses on developing a sentiment analysis system using advanced machine learning techniques. The goal is to analyze textual data to determine the underlying sentiment, which can be classified as positive, negative, or neutral.')
 st.subheader('Enter your comment or review to classify your sentiment')
@@ -15,7 +21,8 @@ comment = st.text_input('Comment: ')
 btn = st.button('Predict')
 if btn and comment:
     comment_list = [comment]
-    sentiment = model.predict(comment_list)    
+    comm_vector = cv.transform(comment_list).toarray()
+    sentiment = model.predict(comm_vector)    
     if sentiment == 1:
         st.success('Positive Sentiment 😊')
     elif sentiment == -1:
